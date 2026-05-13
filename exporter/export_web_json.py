9f5a4e428f0efeb428d1fcd266ef6ac8a8340f7e
PaulElDatos <poliwetox98@gmail.com>
2026-05-13 13:52:04 +0200

    web: update frontend 2026-05-13 13:52:03

--- exporter/export_web_json.py
+++ exporter/export_web_json.py
@@ -71,7 +71,7 @@ MODULES = {
         "title": "M5_RULES_M2M3M4",
         "description": "Reglas finales que combinan M2, M3 y M4.",
         "where": "hypothesis_code REGEXP '^M5(_|$)'",
-        "preferred_signals": ["M2_S_WEAK", "M3_S_HEALTHY", "M4_S_HEALTHY"],
+        "preferred_signals": ["M2_S_WEAK", "M5_HEALTHY"],
     },
     "M6": {
         "title": "M6_MACRO_BLOCK_CONSENSUS",
